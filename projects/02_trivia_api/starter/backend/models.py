58c6c03a8741772ee40cd74b036f467ff35968e2
GSGrivas <georgio.grivas@gmail.com>
2022-09-16 14:56:57 +0200

    Update models.py

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -3,11 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = os.environ.get('DATABASE_NAME')
-localhost = os.environ.get('LOCALHOST')
-user = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-database_path = "postgresql://{}:{}@{}/{}".format(user, password, localhost, database_name)
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+DB_USER = os.getenv('DB_USER', 'postgres')  
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
+DB_NAME = os.getenv('DB_NAME', 'trivia')  
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 db = SQLAlchemy()
 
